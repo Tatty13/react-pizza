0a6422f5c4fbed9c7cfda5f4309a54b3e0a3c20f
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-04-21 14:52:27 +0300

    feat: add the functionality to check an active category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+
 function Categories() {
+  const [activeIdx, setActiveIdx] = useState(3);
+
+  const categoryTypes = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+  ];
+
+  const categoryElems = categoryTypes.map((category, i) => (
+    <li
+      key={i}
+      onClick={() => setActiveIdx(i)}
+      className={activeIdx === i ? 'active' : ''}>
+      {category}
+    </li>
+  ));
+
   return (
     <div className='categories'>
-      <ul>
-        <li className='active'>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
-      </ul>
+      <ul>{categoryElems}</ul>
     </div>
   );
 }
