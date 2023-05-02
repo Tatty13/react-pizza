818232458d61ee8fa499661123537061a95c1e88
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-02 21:56:13 +0300

    feat: focus search input after clearing

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,17 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 
 import styles from './Search.module.scss';
 import SearchContext from '../../contexts/SearchContext';
 
 function Search() {
   const { searchValue, setSearchValue } = useContext(SearchContext);
+  const inputRef = useRef();
 
   function handleChange(e) {
     setSearchValue(e.target.value);
   }
 
-  function clearInput() {
+  function handleClearClick() {
     setSearchValue('');
+    inputRef.current.focus();
   }
 
   return (
@@ -25,6 +27,7 @@ function Search() {
         <path d='M448.3,424.7L335,311.3c20.8-26,33.3-59.1,33.3-95.1c0-84.1-68.1-152.2-152-152.2c-84,0-152,68.2-152,152.2  s68.1,152.2,152,152.2c36.2,0,69.4-12.7,95.5-33.8L425,448L448.3,424.7z M120.1,312.6c-25.7-25.7-39.8-59.9-39.8-96.3  s14.2-70.6,39.8-96.3S180,80,216.3,80c36.3,0,70.5,14.2,96.2,39.9s39.8,59.9,39.8,96.3s-14.2,70.6-39.8,96.3  c-25.7,25.7-59.9,39.9-96.2,39.9C180,352.5,145.8,338.3,120.1,312.6z' />
       </svg>
       <input
+        ref={inputRef}
         className={styles.input}
         placeholder='Поиск пиццы...'
         onChange={handleChange}
@@ -33,7 +36,7 @@ function Search() {
       {searchValue && (
         <svg
           className={styles.closeIcon}
-          onClick={clearInput}
+          onClick={handleClearClick}
           viewBox='0 0 48 48'
           xmlns='http://www.w3.org/2000/svg'>
           <path d='M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z' />
