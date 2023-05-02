d7c0956bfd2a60f5c4de5b8d71fd5b5cbb47d4c6
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-02 20:38:42 +0300

    refactor: add filter slice to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import filter from './slices/filterSlice';
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    filter,
+  },
 });
 
 export { store };
