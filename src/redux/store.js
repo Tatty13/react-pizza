92d1399790757b95e9a138dc62219015f6fd0929
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-16 21:27:27 +0300

    refactor: add pizzas slice to the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import filter from './slices/filterSlice';
 import cart from './slices/cartSlice';
+import pizzas from './slices/pizzasSlice';
 
 const store = configureStore({
   reducer: {
     filter,
     cart,
+    pizzas,
   },
 });
 
