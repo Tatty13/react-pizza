9825d3cff9115be59609d0127eaff2766f949217
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-16 21:24:14 +0300

    feat: create ErrorInfo component, add styles for it

---
+++ src/components/ErrorInfo/index.jsx
@@ -0,0 +1,17 @@
+import styles from './ErrorInfo.module.scss';
+
+function ErrorInfo() {
+  return (
+    <div className={styles.error}>
+      <h2 className={styles.errorTitle}>
+        Произошла ошибка <span>😕</span>
+      </h2>
+      <p className={styles.errorText}>
+        К сожалению, не удалось загрузить пиццы.
+      </p>
+      <p className={styles.errorText}>Попробуйте повторить попытку познее.</p>
+    </div>
+  );
+}
+
+export default ErrorInfo;
