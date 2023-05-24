e12859aadcc35fd676cea17e8d95702b9afc1977
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-24 17:14:55 +0300

    Updates

---
+++ static/js/NotFound.e75f4cc2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,a,r){r.r(a);var t=r(2253),u=r(184);a.default=function(){return(0,u.jsx)(t.vr,{})}}}]);
+//# sourceMappingURL=NotFound.e75f4cc2.chunk.js.map
