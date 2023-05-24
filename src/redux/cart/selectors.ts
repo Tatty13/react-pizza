39db0e54e45831fe08abdf67d77780ad1cc35f23
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-24 21:09:21 +0300

    fix: add type to the return for the getCartFromLS function, return initial cart state if there is no cart in the local storage

--- src/redux/cart/selectors.ts
+++ src/redux/cart/selectors.ts
@@ -2,4 +2,4 @@ import { RootState } from '../store';
 
 export const selectCart = (state: RootState) => state.cart;
 export const selectItemsById = (id: string) => (state: RootState) =>
-  state.cart.items.filter((item) => item.id === id);
+  state.cart.items?.filter((item) => item.id === id);
--- src/utils/getCartFromLS.ts
+++ src/utils/getCartFromLS.ts
@@ -1,9 +1,12 @@
-const getCartFromLS = () => {
+import { initialState } from '../redux/cart/slice';
+import { ICartSliceState } from '../redux/cart/types';
+
+const getCartFromLS = (): ICartSliceState => {
   const cart = localStorage.getItem('cart');
   if (cart) {
     return JSON.parse(cart);
   }
-  return {};
+  return initialState;
 };
 
 export default getCartFromLS;
